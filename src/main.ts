6159fd377ea26bc4898d51febbe08a006b53f45c
Dennis Le <letungson76@gmail.com>
2025-11-01 15:17:31 +1100

    - add openapi document

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Elysia from "elysia";
 import config from "./config";
 import { testConnection } from "./database";
 import { user } from "./modules/user";
+import openapi from "@elysiajs/openapi";
+import z from "zod";
 
 const { APP_PORT, APP_HOST } = config;
 let app: Elysia | null = null;
@@ -32,10 +34,42 @@ async function main() {
 
   try {
     app = new Elysia()
+      .use(
+        openapi({
+          mapJsonSchema: {
+            zod: z.toJSONSchema,
+          },
+          documentation: {
+            info: {
+              title: "Sample ElysiaJS RESTful API",
+              version: "1.0.0",
+            },
+            components: {
+              securitySchemes: {
+                bearerAuth: {
+                  type: "http",
+                  scheme: "bearer",
+                  bearerFormat: "JWT",
+                },
+              },
+            },
+            tags: [
+              {
+                name: "Authentication",
+                description: "Authentication endpoints",
+              },
+              { name: "User", description: "User endpoints" },
+            ],
+          },
+        })
+      )
       .use(user)
-      .get("/", () => "Hello World")
+      .get("/", () => "Hello World", { detail: { hide: true } })
       .listen(APP_PORT);
     console.log(`✅ Server is running on http://${APP_HOST}:${APP_PORT}`);
+    console.log(
+      `✅ OpenAPI document is running on http://${APP_HOST}:${APP_PORT}/openapi`
+    );
   } catch (e) {
     console.log(`Exception caught at main(): ${e}`);
     process.exit(1);
